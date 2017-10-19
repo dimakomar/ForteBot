35ba3d060e6ba29adee755b84d4d4c8a848c0b66
Dima Komar <dmytro.komar@gpshopper.com>
2017-10-19 20:56:24 +0300

    added rest_framework

--- fortebot/views.py
+++ fortebot/views.py
@@ -1,7 +1,15 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.http import HttpResponse
 from .errors import *
+import json
 
+@api_view(['GET', 'POST'])
 def messageSent(request):
-    print(request.body)
-    return success_response()
+    if request.method == 'POST':
+        print(request.body)
+        return success_response()
+    else:
+        return success_response()
 
