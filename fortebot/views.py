4bad8c5d12656f558102a96ecbf64474e914f07f
Dima Komar <dmytro.komar@gpshopper.com>
2017-10-19 21:18:22 +0300

    added challenge fallback

--- fortebot/views.py
+++ fortebot/views.py
@@ -1,15 +1,14 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
+from django.http import JsonResponse
 from .errors import *
 import json
 
 @api_view(['GET', 'POST'])
 def messageSent(request):
     if request.method == 'POST':
-        print(request.body)
-        return success_response()
+        return JsonResponse(request.data)
     else:
         return success_response()
 
