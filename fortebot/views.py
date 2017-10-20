e090c000f2fc06204928e5c2b3834ad3a50f8974
Dima Komar <dmytro.komar@gpshopper.com>
2017-10-20 18:43:15 +0300

    addded repeating request

--- fortebot/views.py
+++ fortebot/views.py
@@ -8,18 +8,18 @@ from fortebot.settings import SLACK_BOT_TOKEN
 import json
 import os
 import jwt
-from datetime import datetime
-from threading import Timer
+import schedule
+import time
 
-x=datetime.today()
-y=x.replace(day=x.day+0, hour=0, minute=0, second=3, microsecond=0)
-delta_t=y-x
 
-secs=delta_t.seconds+1
 
-def hello_world():
-    print("hello world")
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    
+    return success_response()
 
+def hello():
+    print("hello")
 
 
 def send_msg():
@@ -27,8 +27,6 @@ def send_msg():
 
 @api_view(['GET', 'POST'])
 def messageSent(request):
-    t = Timer(secs, hello_world)
-    t.start()
 
     if request.method == 'POST':
         path = os.path.join('noname')
@@ -36,40 +34,50 @@ def messageSent(request):
             encoded_token = myfile.read()
             decoded = jwt.decode(encoded_token, 'hello', algorithms=['HS256'])
             slack_token = decoded['some']
-        sc = SlackClient(slack_token)
-        user_channel = sc.api_call(
-            "im.open",
-            user=request.data['event']['user'],
-        )            
-
-        sc.api_call(
-            "chat.postEphemeral",
-            channel=user_channel['channel']['id'],
-            user=request.data['event']['user'],
-            text="you just said " + request.data['event']['text'] + " :bear:"
-        )
+            sc = SlackClient(slack_token)
+            user_channel = sc.api_call(
+                "im.open",
+                user=request.data['event']['user'],
+            )        
+            sc.api_call(
+                "chat.postEphemeral",
+                channel=user_channel['channel']['id'],
+                user=request.data['event']['user'],
+                text="you just said " + request.data['event']['text'] + " :bear:"
+            )
 
         return JsonResponse(request.data)
     else:
         return success_response()
 
 
-def open_channel_and_send(message):
+def open_channel_and_send():
+    
+
     path = os.path.join('noname')
     with open(path , 'r') as myfile:
         encoded_token = myfile.read()
         decoded = jwt.decode(encoded_token, 'hello', algorithms=['HS256'])
         slack_token = decoded['some']
+        
     sc = SlackClient(slack_token)
     user_channel = opened_dm = sc.api_call(
         "im.open",
         user='U7F85AA80',
     )            
+    user_ss = sc.api_call(
+                "users.list"
+    )
+    print(user_ss)    
 
     sc.api_call(
         "chat.postEphemeral",
         channel=user_channel['channel']['id'],
         user='U7F85AA80',
-        text=message
+        text=user_ss
     )
 
+schedule.every(15).seconds.do(open_channel_and_send)
+while True:
+    schedule.run_pending()
+    time.sleep(1) 
