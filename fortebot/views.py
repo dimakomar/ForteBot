aafc8cc42b9c1ff4262c67cd9df0a164a2760cb0
Dima Komar <dmytro.komar@gpshopper.com>
2017-10-20 12:52:18 +0300

    added slackclient

--- fortebot/views.py
+++ fortebot/views.py
@@ -3,12 +3,31 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse
 from .errors import *
+from slackclient import SlackClient
 import json
+import os
+
+slack_token = ""
 
 @api_view(['GET', 'POST'])
 def messageSent(request):
     if request.method == 'POST':
+        path = os.path.join('noname')
+        with open(path , 'r') as myfile:
+            slack_token = myfile.read()
+        print(slack_token)
         return JsonResponse(request.data)
     else:
         return success_response()
 
+
+
+
+sc = SlackClient(slack_token)
+
+sc.api_call(
+  "chat.postEphemeral",
+  channel="#python",
+  text="Hello from Python! :tada:",
+  user="U0XXXXXXX"
+)
