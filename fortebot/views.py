c30941c1f99d17afc76983cc5a38cf8eb6e19956
Dima Komar <dmytro.komar@gpshopper.com>
2017-10-23 15:37:33 +0300

    added messaging to everyone alive

--- fortebot/views.py
+++ fortebot/views.py
@@ -8,46 +8,70 @@ from fortebot.settings import SLACK_BOT_TOKEN
 import json
 import os
 import jwt
-
-@api_view(['GET', 'POST'])
-def hello_world(request):
-    
-    return success_response()
-
-def hello():
-    print("hello")
-
-
-def send_msg():
-    open_channel_and_send("repeating message")
+from .user import User
 
 @api_view(['GET', 'POST'])
 def vote(request):
     if request.method == 'POST':
-        print(request.data)
-    return success_response()
+        tkn = getToken()
+        sc = SlackClient(tkn)
+        user_list = sc.api_call(
+            "users.list"
+        )
+        
+        members_array = user_list["members"]
+        
+        ids_array = []
+        for member in members_array:
+            ids_array.append(member['id'])
+
+        real_users = []
+        for user_id in ids_array:
+            user_channel = sc.api_call(
+                "im.open",
+                user=user_id,
+            )
+
+            if user_channel['ok'] == True:
+                # User()
+                real_users.append(User(user_id, user_channel['channel']['id']) )
+    
+        print(real_users)
+        for user in real_users:    
+            sc.api_call(
+                "chat.postEphemeral",
+                channel=user.dm_channel,
+                user=user.user_id,
+                text="message for everyone on channel"
+            )
+    return success_response(user_list)
 
 @api_view(['GET', 'POST'])
 def messageSent(request):
 
     if request.method == 'POST':
-        path = os.path.join('noname')
-        with open(path , 'r') as myfile:
-            encoded_token = myfile.read()
-            decoded = jwt.decode(encoded_token, 'hello', algorithms=['HS256'])
-            slack_token = decoded['some']
-            sc = SlackClient(slack_token)
-            user_channel = sc.api_call(
-                "im.open",
-                user=request.data['event']['user'],
-            )        
-            sc.api_call(
-                "chat.postEphemeral",
-                channel=user_channel['channel']['id'],
-                user=request.data['event']['user'],
-                text="you just said " + request.data['event']['text'] + " :bear:"
-            )
+        tkn = getToken()
+        sc = SlackClient(tkn)
+        user_channel = sc.api_call(
+            "im.open",
+            user=request.data['event']['user'],
+        )        
+        sc.api_call(
+            "chat.postEphemeral",
+            channel=user_channel['channel']['id'],
+            user=request.data['event']['user'],
+            text="you just said " + request.data['event']['text'] + " :bear:"
+        )
 
         return JsonResponse(request.data)
     else:
         return success_response()
+
+
+def getToken():
+    path = os.path.join('noname')
+    with open(path , 'r') as myfile:
+        encoded_token = myfile.read()
+        decoded = jwt.decode(encoded_token, 'hello', algorithms=['HS256'])
+        return decoded['some']
+            
