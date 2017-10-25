0d4755bf1d56d8d859d74ddf68a5632ae0823441
Dima Komar <dmytro.komar@gpshopper.com>
2017-10-25 14:12:58 +0300

    another json fix

--- fortebot/views.py
+++ fortebot/views.py
@@ -19,7 +19,7 @@ def anonymous_feedback(request):
     sc.api_call(
         "chat.postMessage",
         channel="G7NUMC5FA",
-        text=request.data['text'][0]
+        text=request.data['text']
     )    
     return success_response()
 
