b33f714338c323c2bbbd824ed83403073585379a
Dima Komar <dmytro.komar@gpshopper.com>
2017-10-31 17:23:23 +0200

    fixed eerror

--- fortebot/views.py
+++ fortebot/views.py
@@ -169,7 +169,7 @@ def start_rating_vote(request, msg):
         open('last_vote_name', 'w').close()
         with open("last_vote_name", "a") as last_vote_name_file:
             last_vote_name_file.write(request.data['text'] if request.data['text'] != "" else "Temperature vote") 
-        send_msg_to_all(sc, request, "".join([msg]))
+        send_msg_to_all(sc, request, "".join([msg, " - please reply with `/rate`"]))
     else:
         send_ephemeral_msg(sc,request.data['user_id'],request.data['channel_id'],settings.BAD_CHANNEL_PHRASE)
     return HttpResponse()
@@ -207,7 +207,7 @@ def send_msg(sc, real_users, req, msg):
 def open_channel_if_needed(sc, request): 
     return sc.api_call(
         "im.open",
-        user=request.data['event']['user'],
+        user=request.data['user_id'],
     )      
 
 def getToken():
