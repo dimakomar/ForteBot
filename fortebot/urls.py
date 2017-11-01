21db193f98c304474e7ee8c2c11b668a36650c78
Dima Komar <dmytro.komar@gpshopper.com>
2017-11-01 18:40:19 +0200

    api test

--- fortebot/urls.py
+++ fortebot/urls.py
@@ -21,8 +21,8 @@ import os
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^slack/', views.sent_message),
-    url(r'^temperature_vote', views.start_temperature_vote),
-    url(r'^forte_vote', views.start_rating_vote),
+    url(r'^temperature_vote', views.temperature_vote),
+    url(r'^forte_vote', views.rating_vote),
     url(r'^anonymous_feedback/', views.anonymous_feedback),
     url(r'^anonymous_msg_random', views.anonymous_msg_random),
     url(r'^forte_help/', views.help),
