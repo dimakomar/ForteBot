3c3a8d690103fa504dcaff62405d6ad95d039ab7
Dima Komar <dmytro.komar@gpshopper.com>
2017-11-01 13:11:33 +0200

    fixed tabs

--- fortebot/views.py
+++ fortebot/views.py
@@ -104,13 +104,13 @@ def question_vote(request):
 
 @api_view(['POST'])
 def temperature_vote(request):
-start_rating_vote(request,settings.VOTE_PHRASE)
-return HttpResponse()
+    start_rating_vote(request,settings.VOTE_PHRASE)
+    return HttpResponse()
 
 @api_view(['POST'])
 def rating_vote(request):
-start_rating_vote(request,"".join([request.data["text"], settings.TEXT_VOTE_PHRASE]))
-return HttpResponse()
+    start_rating_vote(request,"".join([request.data["text"], settings.TEXT_VOTE_PHRASE]))
+    return HttpResponse()
 
 @api_view(['POST'])
 def sent_message(request):
