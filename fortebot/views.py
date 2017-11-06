d7802022a74178a346f29b51539ce217cc646c43
Dima Komar <dmytro.komar@gpshopper.com>
2017-11-06 14:13:11 +0200

    fixed syntax error

--- fortebot/views.py
+++ fortebot/views.py
@@ -105,11 +105,11 @@ def temperature_vote(request):
 
 @api_view(['POST'])
 def rating_vote(request):
-    if request.data["text"] == "" or if request.data["text"] == " ":
+    if request.data["text"] == "" or request.data["text"] == " ":
         tkn = getToken()
         sc = SlackClient(tkn)  
         user_channel = open_events_api_channel_if_needed(sc, request)
-        send_ephemeral_msg(sc,usr,channel, "You've been a step away from huge fail (starting vote with empty message) please check `/help_forte_bot`) 
+        send_ephemeral_msg(sc,usr,channel, "You've been a step away from huge fail (starting vote with empty message) please check `/help_forte_bot`") 
         return HttpResponse()
     start_rating_vote(request,"".join([request.data["text"], settings.TEXT_VOTE_PHRASE]))
     return HttpResponse()
@@ -118,9 +118,9 @@ def rating_vote(request):
 def start_question_vote(request):
     tkn = getToken()
     sc = SlackClient(tkn)
-    if request.data["text"] == "" or if request.data["text"] == " ":
+    if request.data["text"] == "" or request.data["text"] == " ":
         user_channel = open_events_api_channel_if_needed(sc, request)
-        send_ephemeral_msg(sc,usr,channel, "You've been a step away from huge fail (starting vote with empty message) please check `/help_forte_bot`) 
+        send_ephemeral_msg(sc,usr,channel, "You've been a step away from huge fail (starting vote with empty message) please check `/help_forte_bot`") 
         return HttpResponse()
 
     if request.data['channel_id'] == settings.PRIVATE_CHANNEL:
