c895bbc0cf62d6c69779b3dd8703a0a3d0d7601e
Dima Komar <dmytro.komar@gpshopper.com>
2017-11-28 15:47:36 +0200

    removed database ref

--- fortebot/settings.py
+++ fortebot/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'fortebot.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, 'yourdatabasename.db'),
-    }
+    #  'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(PROJECT_DIR, 'yourdatabasename.db'),
+    # }
 }
 
 
