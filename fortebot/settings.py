501be7490affa991a9f5a18ac046a675d0c3e81c
Dima Komar <dmytro.komar@gpshopper.com>
2017-11-28 15:45:49 +0200

    added static

--- fortebot/settings.py
+++ fortebot/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'fortebot.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    #  'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(PROJECT_DIR, 'yourdatabasename.db'),
-    # }
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, 'yourdatabasename.db'),
+    }
 }
 
 
@@ -124,7 +124,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
