a2a309033ac7a8a0446011bee12fac2d1a40b9c2
Dima Komar <dmytro.komar@gpshopper.com>
2018-04-26 16:40:50 +0300

    trig

--- bot/views.py
+++ bot/views.py
@@ -198,7 +198,7 @@ def start_due(request):
     # trigger = OrTrigger([CronTrigger(day_of_week='wed', hour=15, minute=43, second=0),
     #                  CronTrigger(day_of_week='wed', hour=15, minute=42, second=0)])
     # scheduler.add_job(job, 'date', run_date=datetime(2018,4,26,15,30,0)) 
-    scheduler.add_job(job, 'date', run_date='2018-04-26 16:40:20', args=["U6DDYBZ6Z"])
+    scheduler.add_job(job, 'date', run_date='2018-04-26 16:43:20', args=["U6DDYBZ6Z"])
     scheduler.start()
     return HttpResponse()
 
