636668e48728fdb151973a026345c32c12892a1d
Dima Komar <dmytro.komar@gpshopper.com>
2018-04-27 18:39:23 +0300

    test

--- bot/views.py
+++ bot/views.py
@@ -198,8 +198,8 @@ def start_due(request):
     # trigger = OrTrigger([CronTrigger(day_of_week='wed', hour=15, minute=43, second=0),
     #                  CronTrigger(day_of_week='wed', hour=15, minute=42, second=0)])
     # scheduler.add_job(job, 'date', run_date=datetime(2018,4,26,15,30,0)) 
-    scheduler.add_job(job, 'date', run_date='2018-04-27 18:30:10', args=["U7F85AA80"])
-    scheduler.add_job(job, 'date', run_date='2018-04-27 18:30:30', args=["U7F85AA80"])
+    scheduler.add_job(job, 'date', run_date='2018-04-27 18:43:10', args=["U7F85AA80"])
+    scheduler.add_job(job, 'date', run_date='2018-04-27 18:43:30', args=["U7F85AA80"])
 
     scheduler.start()  
     return HttpResponse()
@@ -216,7 +216,8 @@ def job(user_id):
     # print(job_request.data)
     tkn = getToken()
     sc = SlackClient(tkn)  
-    channel = open_channel_if_needed(sc,"D7LTDA0TF")
+    channel = open_channel_if_needed(sc,"U7L829RCH")
+    print(channel)
     user = sc.api_call(
         "users.profile.get",
         user="U7F85AA80"
