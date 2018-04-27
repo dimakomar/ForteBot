5c1989cb42730df12b9162aaf765e3462efd92f5
Dima Komar <dmytro.komar@gpshopper.com>
2018-04-27 15:46:13 +0300

    test

--- bot/views.py
+++ bot/views.py
@@ -198,8 +198,9 @@ def start_due(request):
     # trigger = OrTrigger([CronTrigger(day_of_week='wed', hour=15, minute=43, second=0),
     #                  CronTrigger(day_of_week='wed', hour=15, minute=42, second=0)])
     # scheduler.add_job(job, 'date', run_date=datetime(2018,4,26,15,30,0)) 
-    scheduler.add_job(job, 'date', run_date='2018-04-26 17:55:10', args=["U7NCK22KW"])
-    scheduler.add_job(job, 'date', run_date='2018-04-26 17:55:10', args=["U7F85AA80"])
+    scheduler.add_job(job, 'date', run_date='2018-04-27 15:49:10', args=["U7NCK22KW"])
+    scheduler.add_job(job, 'date', run_date='2018-04-27 15:49:10', args=["U7NCK22KW"])
+
     scheduler.start()
     return HttpResponse()
 
@@ -216,7 +217,11 @@ def job(user_id):
     tkn = getToken()
     sc = SlackClient(tkn)  
     channel = open_channel_if_needed(sc,user_id)
-    send_normal_duty_msg(sc,channel,"yo meatbag")
+    user = sc.api_call(
+        "users.profile.get",
+        user=user_id
+    )
+    send_normal_duty_msg(sc,channel,''.join(["Hey, you're on duty on the 3rd floor along with " + user["profile"]["real_name_normalized"] + "@" + user["profile"]["display_name"]))
 
 @api_view(['POST'])
 def reply(request):
