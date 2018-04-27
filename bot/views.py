a87d446ff4e071c4ece6c99978b8235a7c143a39
Dima Komar <dmytro.komar@gpshopper.com>
2018-04-27 20:03:50 +0300

    test

--- bot/views.py
+++ bot/views.py
@@ -197,8 +197,8 @@ def start_due(request):
     # trigger = OrTrigger([CronTrigger(day_of_week='wed', hour=15, minute=43, second=0),
     #                  CronTrigger(day_of_week='wed', hour=15, minute=42, second=0)])
     # scheduler.add_job(job, 'date', run_date=datetime(2018,4,26,15,30,0)) 
-    scheduler.add_job(job, 'date', run_date='2018-04-27 20:02:10', args=["U7F85AA80", "U7F85AA80"])
-    scheduler.add_job(job, 'date', run_date='2018-04-27 20:02:30', args=["U7F85AA80", "U7F85AA80"])
+    scheduler.add_job(job, 'date', run_date='2018-04-27 20:05:10', args=["U7F85AA80", "U7F85AA80"])
+    scheduler.add_job(job, 'date', run_date='2018-04-27 20:05:30', args=["U7F85AA80", "U7F85AA80"])
 
     scheduler.start()  
     return HttpResponse()
@@ -217,7 +217,7 @@ def job(user_id, with_user_id):
     # )
     question_attachments = [
         {
-            "text": text,
+            "text": "Hey, you're on duty on the 3rd floor",
             "color": "#3AA3E3",
             "attachment_type": "default",
             "callback_id": "game_selection",
