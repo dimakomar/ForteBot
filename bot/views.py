db85102ec7b3d4e46a7f61560c84ee17ed2a16a5
Dima Komar <dmytro.komar@gpshopper.com>
2018-05-08 19:35:29 +0300

    test

--- bot/views.py
+++ bot/views.py
@@ -201,10 +201,10 @@ def start_due(request):
     # scheduler.add_job(job, 'date', run_date='2018-05-08 16:20:00', args=["U6DDYBZ6Z", "U0L2U6AQ2", False])
     # scheduler.add_job(job, 'date', run_date='2018-05-08 16:20:00', args=["U0L2U6AQ2", "U6DDYBZ6Z", False])
 
-    # scheduler.add_job(job, 'date', run_date='2018-05-08 16:20:00', args=["U04RZ1L76", "U3B9M8SAJ", True])
+    scheduler.add_job(job, 'date', run_date='2018-05-08 19:38:20', args=["U6DDYBZ6Z", "U6DDYBZ6Z", True])
     # scheduler.start()
     #   
-    configureThirdFloor(["2018-05-08 19:33:00","2018-05-08 19:33:10","2018-05-08 19:33:20"],"U6DDYBZ6Z", "U6DDYBZ6Z")
+    configureThirdFloor(["2018-05-08 19:38:00","2018-05-08 19:38:10","2018-05-08 19:38:20"],"U6DDYBZ6Z", "U6DDYBZ6Z")
     return HttpResponse()
 
 def configureThirdFloor(dates, id, second_id):
