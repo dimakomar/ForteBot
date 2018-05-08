2df86a2f1bd520d957f3133863a44c0cc8674281
Dima Komar <dmytro.komar@gpshopper.com>
2018-05-08 15:54:09 +0300

    test

--- bot/views.py
+++ bot/views.py
@@ -206,7 +206,7 @@ def start_due(request):
 
     # scheduler.add_job(job, 'date', run_date='2018-05-08 16:00:00', args=["U04RZ1L76", "U3B9M8SAJ", True])
     print("triggered0")
-    scheduler.add_job(job, 'date', run_date='2018-05-08 16:53:00', args=["U0A27LV3N", "U0L2U6AQ2", False])
+    scheduler.add_job(job, 'date', run_date='2018-05-08 15:56:00', args=["U0A27LV3N", "U0L2U6AQ2", False])
     scheduler.start()  
     return HttpResponse()
 
