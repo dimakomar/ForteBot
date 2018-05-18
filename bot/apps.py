14ea156585208ceaf06709fd9de682573b1917f4
Dima Komar <dmytro.komar@gpshopper.com>
2018-05-18 18:55:04 +0300

    test

--- bot/apps.py
+++ bot/apps.py
@@ -1,6 +1,16 @@
+import sys
 from django.apps import AppConfig
+
 
 class BotConfig(AppConfig):
     name = 'bot'
     verbose_name = "Forte Bot"
+    def ready(self):
+        if 'runserver' not in sys.argv:
+            return True
+        # you must import your modules here 
+        # to avoid AppRegistryNotReady exception 
+        from bot import jobs
+        jobs.start_due()
+        # startup code here
         
