00f917bfbf4e20b9ab419212d13b7108eacd5f88
Dima Komar <dmytro.komar@gpshopper.com>
2018-05-23 14:10:36 +0300

    test

--- bot/apps.py
+++ bot/apps.py
@@ -6,10 +6,10 @@ class BotConfig(AppConfig):
     name = 'bot'
     verbose_name = "Forte Bot"
     def ready(self):
-        if 'bot.wsgi' in sys.argv:
-            from bot import jobs
-            print("aaaa")}
-            jobs.start_due()            
+        # if 'bot.wsgi' in sys.argv:
+        from bot import jobs
+            # print("aaaa")}
+        jobs.start_due()            
         # you must import your modules here 
         # to avoid AppRegistryNotReady exception 
         
