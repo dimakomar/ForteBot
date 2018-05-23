399a2477f634d468189b4b2c3bbcb872f9b7555b
Dima Komar <dmytro.komar@gpshopper.com>
2018-05-23 12:32:10 +0300

    test

--- bot/apps.py
+++ bot/apps.py
@@ -6,7 +6,7 @@ class BotConfig(AppConfig):
     name = 'bot'
     verbose_name = "Forte Bot"
     def ready(self):
-        if 'runserver' not in sys.argv:
+        if 'python bot/jobs.py' not in sys.argv:
             return True
         # you must import your modules here 
         # to avoid AppRegistryNotReady exception 
