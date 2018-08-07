a4f507c3d856783b05a7a740fa2ae1465e920a12
Dima Komar <kmr.dma@gmail.com>
2018-08-07 12:58:43 +0300

    added triggers

--- bot/jobs.py
+++ bot/jobs.py
@@ -705,6 +705,11 @@ def start_due():
     scheduler.add_job(food_job, 'date', run_date='2018-08-01 17:00:00', args=["Wednesday"])
     scheduler.add_job(food_job, 'date', run_date='2018-08-02 17:00:00', args=["Thursday"])
     scheduler.add_job(food_job, 'date', run_date='2018-08-03 17:00:00', args=["Friday"])
+
+    scheduler.add_job(food_job, 'date', run_date='2018-08-07 17:00:00', args=["Tuesday"])
+    scheduler.add_job(food_job, 'date', run_date='2018-08-08 17:00:00', args=["Wednesday"])
+    scheduler.add_job(food_job, 'date', run_date='2018-08-09 17:00:00', args=["Thursday"])
+    scheduler.add_job(food_job, 'date', run_date='2018-08-10 17:00:00', args=["Friday"])
        
     print("sheduler trigageredddd")
     scheduler.start()
