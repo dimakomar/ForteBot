d87021df91766f49d4b0e7e50d9bac04c2f7eae2
dimakomar <kmr.dma@gmail.com>
2019-05-10 18:24:37 +0300

    friday fix

--- bot/jobs.py
+++ bot/jobs.py
@@ -57,8 +57,8 @@ def start_due():
     scheduler.add_job(order_meeting_room, 'cron', hour= '12', minute='00', second='00', args=[])
 
     scheduler.add_job(stop_food_ordering, 'cron', hour= '11', minute='00', second='05', args=[])
-    scheduler.add_job(get_food_job_friday, 'cron', hour= '18', minute='00', second='00', args=[])
-    scheduler.add_job(get_food_job, 'cron', hour= '16', minute='30', second='05', args=[])
+    scheduler.add_job(get_food_job_friday, 'cron', hour= '18', minute='30', second='00', args=[])
+    scheduler.add_job(get_food_job, 'cron', hour= '15', minute='00', second='05', args=[])
     
     scheduler.start()
 
@@ -126,7 +126,7 @@ def get_food_job_friday():
     list_of_hashes = sheet.get_all_records()
 
     tomorrow_day = datetime.date.today() + datetime.timedelta(days=3)
-    tomorrow = datetime.datetime.now(timezone('Europe/Kiev')).replace(month = tomorrow_day.month, day=tomorrow_day.day, hour=11, minute=00)
+    tomorrow = datetime.datetime.now().replace(month = tomorrow_day.month, day=tomorrow_day.day, hour=11, minute=00)
 
     tomorrow_date_str = str(tomorrow)
 
