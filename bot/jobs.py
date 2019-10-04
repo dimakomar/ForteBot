25f4c931912369d4ba49d64c4a5b04d5a8c6101f
dimakomar <kmr.dma@gmail.com>
2019-10-04 20:52:55 +0300

    time changed

--- bot/jobs.py
+++ bot/jobs.py
@@ -57,8 +57,8 @@ def start_due():
     scheduler.add_job(order_meeting_room, 'cron', hour= '12', minute='00', second='00', args=[])
 
     scheduler.add_job(stop_food_ordering, 'cron', hour= '11', minute='00', second='05', args=[])
-    scheduler.add_job(get_food_job_friday, 'cron', hour= '15', minute='00', second='00', args=[])
-    scheduler.add_job(get_food_job, 'cron', hour= '18', minute='0', second='05', args=[])
+    scheduler.add_job(get_food_job, 'cron', hour= '21', minute='00', second='00', args=[])
+    scheduler.add_job(get_food_job_friday, 'cron', hour= '18', minute='0', second='05', args=[])
     
     scheduler.start()
 
